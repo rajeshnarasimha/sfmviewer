6971053bf34c08df453117b7c041003c8a44cb36
nikai97 <nikai97@258dd9c2-bf19-8df1-1899-815ae907d206>
2010-03-19 08:32:11 +0000

    the first drawing works

--- cpp/GLCanvas.h
+++ cpp/GLCanvas.h
@@ -8,61 +8,112 @@
 
 #pragma once
 
+#include <boost/function.hpp>
+#include <boost/optional.hpp>
 #include <QGLWidget>
+#include <gtsam/SimpleCamera.h>
 
-class QtLogo;
+namespace sfmviewer {
 
-class GLCanvas : public QGLWidget
-{
-    Q_OBJECT
+	// the data structure for the view port
+	struct ViewPort {
+		float m_shift[3];  // translation
+		float m_quat[4];   // orientation in quaternion format
+		ViewPort() {}
+		ViewPort(float x, float y, float z, float q1, float q2, float q3, float q4) {
+			m_shift[0] = x; m_shift[1] = y; m_shift[2] = z;
+			m_quat[0] = q1; m_quat[1] = q2; m_quat[2] = q3; m_quat[3] = q4;
+		}
 
-public:
-    GLCanvas(QWidget *parent = 0);
-    ~GLCanvas();
+	};
 
-    // return the minimal size of the window
-    QSize minimumSizeHint() const;
+  // the data structure for 3D points
+	struct Vertex{
+		GLfloat X,Y,Z;
+		Vertex(GLfloat X0, GLfloat Y0, GLfloat Z0) : X(X0), Y(Y0), Z(Z0) {}
+		Vertex() {}
+	};
 
-    // Used by windowing system, uses globals set by size
-    QSize sizeHint() const;
+	// the data structure for the colors of 3D points
+	struct VertexColor{
+		unsigned char r,g,b;
+		VertexColor(unsigned char r0, unsigned char g0, unsigned char b0) : r(r0), g(g0), b(b0) {}
+	};
 
-    // Called from window.cpp to set default size
-    void setSizeHint(int w, int h);
+	// a camera is composed of five vertices
+	struct CameraVertices{
+		Vertex v[5];
+	};
 
-protected:
-    void initializeGL();
-    void paintGL();
-    void resizeGL(int width, int height);
+	class GLCanvas : public QGLWidget
+	{
+		Q_OBJECT
 
-     // record the last mouse position
-    void mousePressEvent(QMouseEvent *event);
+	public:
 
-    // mouse click and drag
-    void mouseMoveEvent(QMouseEvent *event);
+		// callback function types
+		typedef boost::function<void (GLCanvas&)> Callback;
 
-    void DrawStructure();
+		// constructor
+		GLCanvas(QWidget *parent = 0);
 
+		// destructor
+		~GLCanvas();
 
+		// return the minimal size of the window
+		QSize minimumSizeHint() const;
 
-private:
-    QPoint lastPos_;
-    QColor qtGreen;
-    QColor qtPurple;
+		// Used by windowing system, uses globals set by size
+		QSize sizeHint() const;
 
-    int hintWidth_;
-    int hintHeight_;
+		// Called from window.cpp to set default size
+		void setSizeHint(int w, int h);
 
-    // the data structure for the view port
-    struct ViewPort {
-        float m_shift[3];  // translation
-        float m_quat[4];   // orientation in quaternion format
-    };
+		// set up users' callback functions
+		void setCallback(const Callback& fun_draw) { fun_draw_ = fun_draw; }
 
-    // the current opengl viewport
-    ViewPort viewPort_;
+		void setViewPort(const ViewPort& port) { viewPort_ = port; }
 
-    struct Vertex{
-        GLfloat X,Y,Z;
-    };
-    std::vector<Vertex> structure_;
-};
+	protected:
+		// intialize the opengl canvas
+		void initializeGL();
+
+		// paint function
+		void paintGL();
+
+		// callback when the window is resized
+		void resizeGL(int width, int height);
+
+		// load the bunny example
+		void drawBunny();
+
+		// record the last mouse position
+		void mousePressEvent(QMouseEvent *event);
+
+		// mouse click and drag
+		void mouseMoveEvent(QMouseEvent *event);
+
+	private:
+		QPoint lastPos_;
+
+		int hintWidth_;
+		int hintHeight_;
+
+		// the current opengl viewport
+		ViewPort viewPort_;
+
+		// the pointers of callback functions
+		Callback fun_draw_;
+	};
+
+	// draw the 3D structure
+	void drawStructure(const std::vector<Vertex>& structure,
+			const std::vector<VertexColor>& pointColors = std::vector<VertexColor>());
+
+	// draw cameras
+	void drawCameras(const std::vector<CameraVertices>& cameras);
+
+	// backproject four corners of the image to the system coordinate
+	CameraVertices calcCameraVertices(const gtsam::SimpleCamera& camera, const int img_w = 800, const int img_h = 800);
+
+} // namespace sfmviewer
--- cpp/exes/kai01.cpp
+++ cpp/exes/kai01.cpp
@@ -5,5 +5,65 @@
  *       Author: nikai
  *  Description: the most simpler viewer
  */
+#include <fstream>
 
 #include "main.h"
+#include "trackball.h"
+
+
+using namespace std;
+using namespace gtsam;
+using namespace sfmviewer;
+
+#define LINESIZE 81920
+static string filename = "/Users/nikai/borg/sfmviewer/data/StPeter.txt";
+
+static vector<Vertex> structure;
+static vector<VertexColor> pointColors;
+static vector<CameraVertices> cameras;
+
+void sfmviewer::setup(SFMViewer& window)
+{
+	// load the files
+	ifstream is(filename.c_str());
+	string tag;
+	while (is) {
+		is >> tag;
+
+		// load 3D points
+		if (tag == "POINT3") {
+			double x, y, z, r, g, b;
+			is >> x >> y >> z >> r >> g >> b;
+			structure.push_back(Vertex(x,y,z));
+			pointColors.push_back(VertexColor((unsigned char)(r*255)
+					,(unsigned char)(g*255),(unsigned char)(b*255)));
+		}
+
+		// load 3D camera
+		if (tag == "POSE3") {
+			double x, y, z, r11, r12, r13, r21, r22, r23, r31, r32, r33;
+			is >> x >> y >> z
+				 >> r11 >> r12 >> r13
+				 >> r21 >> r22 >> r23
+				 >> r31 >> r32 >> r33;
+			Pose3 pose(Rot3(r11, r12, r13, r21, r22, r23, r31, r32, r33), Point3(x, y, z));
+			SimpleCamera camera(Cal3_S2(120., 1600, 1600), pose);
+			cameras.push_back(calcCameraVertices(camera, 1600, 1600));
+		}
+
+		is.ignore(LINESIZE, '\n');
+	}
+	is.close();
+	cout << "loaded " << structure.size() << " points and " << cameras.size() << " cameras" << endl;
+	cout.flush();
+
+	// set view port
+	ViewPort viewport(-1.93, -57.07, -200, -0.292426, 0.0699906, 0.0326308, 0.953166);
+	//ViewPort viewport(-0., -5., -10., 0., 0., 0., 1.);
+	window.canvas()->setViewPort(viewport);
+}
+
+void sfmviewer::draw(GLCanvas& canvas) {
+	drawStructure(structure, pointColors);
+	drawCameras(cameras);
+}
